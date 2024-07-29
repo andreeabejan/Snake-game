1adb4be10b2e5da4c36d9b9f913051db9a99d184
Bejan Andreea <andreeabejan03@gmail.com>
2024-07-29 19:39:16 +0300

    Added a start up window and a settings menu

--- main.py
+++ main.py
@@ -15,12 +15,102 @@ SNAKE_COLOR = general.SNAKE_COLOR
 FOOD_COLOR = general.FOOD_COLOR
 BACKGROUND_COLOR = general.BACKGROUND_COLOR
 
-def next_turn(snake, food):
+score = 0
+direction = 'down'
 
+def personalize_game():
+    window = Toplevel()
+
+    window.title("Snake game settings")
+    window.resizable(False, False)
+
+    window.geometry("600x400")
+
+    x = 1
+    y = 2
+    z = 3
+    q = 4
+    k = 5
+
+    backgroundColorLabel = Label(window, text="Background color", font=('consolas', 30)).grid(row=0, column=0, columnspan=3)
+    color1 = Radiobutton(window, text="black", variable=x, font=('consolas', 10), value=1, indicatoron=False).grid(row=1,column=0)
+    color2 = Radiobutton(window, text="red", variable=x, font=('consolas', 10), value=2, indicatoron=False).grid(row=1,column=1)
+    color3 = Radiobutton(window, text="blue", variable=x, font=('consolas', 10), value=3, indicatoron=False).grid(row=1, column=3)
+
+    snakeColorLabel = Label(window, text="Snake color", font=('consolas', 30)).grid(row=2, column=0, columnspan=3)
+    color4 = Radiobutton(window, text="black", variable=y, indicatoron=False, value=4, font=('consolas', 10)).grid(row=3, column=0)
+    color5 = Radiobutton(window, text="red" ,variable=y, indicatoron=False, value=5, font=('consolas', 10)).grid(row=3, column=1)
+    color6 = Radiobutton(window, text="blue", variable=y, indicatoron=False, value=6, font=('consolas', 10)).grid(row=3, column=3)
+
+    foodColorLabel = Label(window, text="Food color", font=('consolas', 30)).grid(row=4, column=0, columnspan=3)
+    color7 = Radiobutton(window, text="black", variable=z, indicatoron=False, value=7, font=('consolas', 10)).grid(row=5, column=0)
+    color8 = Radiobutton(window, text="red",variable=z, indicatoron=False, value=8, font=('consolas', 10)).grid(row=5, column=1)
+    color9 = Radiobutton(window, text="blue",variable=z, indicatoron=False, value=9, font=('consolas', 10)).grid(row=5, column=3)
+
+    speedLabel = Label(window, text="Speed", font=('consolas', 30)).grid(row=6, column=0, columnspan=3)
+    speed1 = Radiobutton(window, text="50", variable=q, indicatoron=False, value=10, font=('consolas', 10)).grid(row=7, column=0)
+    speed2 = Radiobutton(window, text="100", variable=q, indicatoron=False, value=11, font=('consolas', 10)).grid(row=7,column=1)
+    speed3 = Radiobutton(window, text="150", variable=q, indicatoron=False, value=12, font=('consolas', 10)).grid(row=7,column=3)
+
+    button = Button(window, text="Start game", command=create_game)
+    button.place(relx=0.3, rely=0.9, anchor="n")
+
+    window.mainloop()
+
+
+def create_game():
+
+    root.destroy()
+
+    window = Tk()
+
+    window.title("Snake game")
+    window.resizable(False, False)
+
+    label = Label(window, text="Score:{}".format(score), font=('consolas', 40))
+    label.pack()
+
+    canvas = Canvas(window, bg=BACKGROUND_COLOR, height=GAME_HEIGHT, width=GAME_WIDTH)
+    canvas.pack()
+
+    window.update()
+
+    #Here, I try to place the game window as much in the center of the screen as possible
+    window_width = window.winfo_width()
+    window_height = window.winfo_height()
+    screen_width = window.winfo_screenwidth()
+    screen_height = window.winfo_screenheight()
+
+    x = int((screen_width / 2) - (window_width / 2))
+    y = int((screen_height / 2) - (window_height / 2))
+
+    window.geometry(f"{window_width}x{window_height}+{x}+{y}")
+
+    #essential line for keyboard events to work,
+    #because initially focus is not on this window
+    window.focus_force()
+
+    #Here I set the keyboard events for changing the snake's directions
+    window.bind('<Left>', lambda event: change_direction('left'))
+    window.bind('<Right>', lambda event: change_direction('right'))
+    window.bind('<Down>', lambda event: change_direction('down'))
+    window.bind('<Up>', lambda event: change_direction('up'))
+
+    snake = Snake(canvas, general)
+    food = Food(canvas, general)
+
+    next_turn(snake, food, canvas, label, window)
+
+    window.mainloop()
+
+
+def next_turn(snake, food, canvas, label, window):
     x, y = snake.coordinates[0]
 
+    global direction
+
     if direction == "up":
-         y -= SPACE_SIZE
+        y -= SPACE_SIZE
 
     elif direction == "down":
 
@@ -36,7 +126,7 @@ def next_turn(snake, food):
 
     snake.coordinates.insert(0, (x, y))
 
-    square = canvas.create_rectangle(x, y, x + SPACE_SIZE, y + SPACE_SIZE, fill = SNAKE_COLOR)
+    square = canvas.create_rectangle(x, y, x + SPACE_SIZE, y + SPACE_SIZE, fill=SNAKE_COLOR)
     snake.squares.insert(0, square)
 
     if x == food.coordinates[0] and y == food.coordinates[1]:
@@ -47,7 +137,7 @@ def next_turn(snake, food):
 
         label.config(text="Score:{}".format(score))
 
-        canvas.delete("food") #via tag
+        canvas.delete("food")  #via tag
 
         food = Food(canvas, general)
     else:
@@ -60,16 +150,17 @@ def next_turn(snake, food):
 
     if check_colisions(snake):
 
-        game_over()
+        game_over(canvas)
 
     else:
 
-        window.after(SPEED, next_turn, snake, food)
+        window.after(SPEED, next_turn, snake, food, canvas, label, window)
 
 
 def change_direction(new_direction):
 
-    global direction #the old direction
+    global direction  #the old direction
+
 
     if new_direction == 'left':
         if direction != 'right':
@@ -88,62 +179,49 @@ def change_direction(new_direction):
 def check_colisions(snake):
     x, y = snake.coordinates[0]
 
-    if x < 0 or x >=  GAME_WIDTH:
+    if x < 0 or x >= GAME_WIDTH:
         print("Game over!")
         return True
     elif y < 0 or y >= GAME_HEIGHT:
         print("Game over!")
         return True
 
-    for body_part in snake.coordinates[1:]: #everything after the head of the snake
+    for body_part in snake.coordinates[1:]:  #everything after the head of the snake
         if x == body_part[0] and y == body_part[1]:
             print("Game over!")
             return True
     return False
 
 
-
-def game_over():
-
+def game_over(canvas):
     canvas.delete(ALL)
-    canvas.create_text((canvas.winfo_width()/2), (canvas.winfo_height()/2), font=('consolas',70), text="GAME OVER", fill="blue", tag="game")
+    canvas.create_text((canvas.winfo_width() / 2), (canvas.winfo_height() / 2), font=('consolas', 70), text="GAME OVER",
+                       fill="blue", tag="game")
 
-window = Tk()
 
-window.title("Snake game")
-window.resizable(False,False)
+root = Tk()
+root.title("Snake game")
+background_image = PhotoImage(file="snake.png")
 
-score = 0
-direction = 'down'
 
-label = Label(window, text="Score:{}".format(score),font=('consolas',40))
-label.pack()
+# Here, I try to place the game window as much in the center of the screen as possible
+window_width = background_image.width()
+window_height = background_image.height()
+screen_width = root.winfo_screenwidth()
+screen_height = root.winfo_screenheight()
 
-canvas = Canvas(window, bg=BACKGROUND_COLOR, height=GAME_HEIGHT, width=GAME_WIDTH)
-canvas.pack()
+x = int((screen_width / 2) - (window_width / 2))
+y = int((screen_height / 2) - (window_height / 2))
 
-window.update()
+root.geometry(f"{window_width}x{window_height}+{x}+{y}")
 
-#Here, I try to place the game window as much in the center of the screen as possible
-window_width = window.winfo_width()
-window_height = window.winfo_height()
-screen_width = window.winfo_screenwidth()
-screen_height = window.winfo_screenheight()
+background_label = Label(root, image=background_image)
+background_label.place(relwidth=1, relheight=1)
 
-x = int((screen_width/2) - (window_width/2))
-y = int((screen_height/2) - (window_height/2))
+button1 = Button(root, text="Play game", command=create_game)
+button1.place(relx=0.3, rely=0.5, anchor="e")
 
-window.geometry(f"{window_width}x{window_height}+{x}+{y}")
+button2 = Button(root, text="Settings", command=personalize_game)
+button2.place(relx=0.8, rely=0.5, anchor="w")
 
-#Here I set the keyboard events for changing the snake's directions
-window.bind('<Left>', lambda event: change_direction('left'))
-window.bind('<Right>', lambda event: change_direction('right'))
-window.bind('<Down>', lambda event: change_direction('down'))
-window.bind('<Up>', lambda event: change_direction('up'))
-
-snake = Snake(canvas, general)
-food = Food(canvas, general)
-
-next_turn(snake,food)
-
-window.mainloop()
+root.mainloop()
